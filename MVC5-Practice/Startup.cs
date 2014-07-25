da048a0d24af7dd76d6078f727788410ff499641
84zume <hideki.hashizume@gmail.com>
2014-07-25 13:51:08 +0900

    テンプレートをそのまま入れる

---
+++ MVC5-Practice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_Practice.Startup))]
+namespace MVC5_Practice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
